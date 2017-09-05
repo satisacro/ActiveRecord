5b506018f182f9c8399dddc9eaa2ee0c1f181c51
Juan Esteban Caicedo <jecaicedop@unal.edu.co>
2017-09-04 22:19:19 -0500

    Creating validation and Active Record Associations

---
+++ app/models/account.rb
@@ -0,0 +1,7 @@
+class Account< ApplicationRecord
+    validates :account_owner, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
+    validates :identification, numericality: { only_integer: true }
+    validates :name, :identification, presence: true
+    validates :identification, uniqueness: true
+    belongs_to:supplier
+end
---
+++ app/models/supplier.rb
@@ -0,0 +1,9 @@
+class Supplier < ApplicationRecord
+    validates :name_supplier, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
+    validates :telephone, length: { is: 10 }
+    validates :telephone, numericality: { only_integer: true }
+    validates :identification, numericality: { only_integer: true }
+    validates :name, :telephone, :identification, presence: true
+    validates :identification, uniqueness: true
+    has_one:account
+end
