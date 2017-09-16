dae4abc625eaeb0ce8ab05a38f76d29b05171655
Raúl Ricardo Orcasitas <rrorcasitash@unal.edu.co>
2017-09-15 23:40:34 -0500

    Update editorial.rb

--- app/models/editorial.rb
+++ app/models/editorial.rb
@@ -1,6 +1,14 @@
 class Editorial < ApplicationRecord
     validates :city, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
     validates :nombre, :cel, :doc, presence: true
+    before_validation :ensure_nombre_has_value
+    
+    private
+    def ensure_nombre_has_a_value
+      if nombre.nil?
+        self.nombre = doc unless doc.blank?
+      end
+    end
     validates :name, length: { minimum: 2 }
     validates :email, length: { maximum: 500 }
 end
