2c2038a84ed354b174233b0052f4d0287c862e80
Raúl Ricardo Orcasitas <rrorcasitash@unal.edu.co>
2017-09-15 23:37:47 -0500

    Update client.rb

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,9 +1,14 @@
 class Client < ApplicationRecord
     validates :nombre, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
+	
     validates :cel, length: { is: 10 }
     validates :cel, numericality: { only_integer: true }
     validates :doc, numericality: { only_integer: true }
-    validates :nombre, :cel, :doc, presence: true
+    after_validation do
+		self.nombre = nombre.capitalize
+	end
+	
+	validates :nombre, :cel, :doc, presence: true
     validates :doc, uniqueness: true
     belongs_to :books 
 end
